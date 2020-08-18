204b0724104344b428b95e14ee8b4e17efaa6caa
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-18 03:50:20 +0000

    update file
    deploy webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,78 @@
 #!/usr/bin/python3
 """ a Fabric script that distributes
     an archive to your web servers """
-from fabric.api import env, run, put, sudo
+from fabric.api import env, run, put, sudo, local
+from datetime import datetime
 import os.path
-from fabric.main import main
 
-env.hosts = ['35.243.141.246', '54.227.41.202']
-env.user = 'ubuntu'
+env.hosts = ["35.243.141.246", "54.227.41.202"]
+
+
+def do_pack():
+    """ script that generates a .tgz archive """
+    date_now = datetime.utcnow()
+    year = date_now.year
+    month = date_now.month
+    day = date_now.day
+    hour = date_now.hour
+    minute = date_now.minute
+    second = date_now.second
+
+    file_date = "versions/web_static_{}{}{}{}{}{}.tgz".format(year, month, day,
+                                                              hour, minute,
+                                                              second)
+
+    local("sudo mkdir -p versions")
+    local("sudo tar -cvzf {} web_static".format(file_date))
+
+    return (file_date)
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to your web servers """
-    if not archive_path:
+    """Function that distributes an archive to your web servers"""
+    """local: sudo tar -cvzf versions/web_static_202081833632.tgz web_static"""
+
+    if os.path.exists(archive_path) is False:
         return False
-    file_arg = archive_path.split("/")[-1]
-    new = '/data/web_static/release' + "{}".format(file_arg.split('.')[0])
-    current = 'data/web_static/current'
-    try:
-        put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}/'.format(new))
-        run('sudo tar -xzf {} -C {}/'.format(file_arg, new))
-        run('sudo rm /tmp/{}'.format(file_arg))
-        run('sudo mv {}/web_static {}/'.format(new))
-        run('sudo rm -rf {}'.format(current))
-        run('sudo ln -s {}/ {}'.format(new, current))
-        return True
-    except:
-        return False
+
+    status = True
+
+    upload = put(archive_path, '/tmp/')
+    if upload.failed:
+        status = False
+
+    archive_name = archive_path.replace("versions/", "")
+    directory_name = archive_name.replace(".tgz", "")
+
+    folder = '/data/web_static/releases/{}/'.format(directory_name)
+
+    directory = run("mkdir -p {}".format(folder))
+    if directory.failed:
+        status = False
+
+    unpack_cmd = 'tar -xzf /tmp/{} -C {}'.format(archive_name, folder)
+    unpack = run(unpack_cmd)
+    if unpack.failed:
+        status = False
+
+    remove = run('rm /tmp/{}'.format(archive_name))
+    if remove.failed:
+        status = False
+
+    move = run('mv {}web_static/* {}'.format(folder, folder))
+    if move.failed:
+        status = False
+
+    remove = run('rm -rf {}web_static'.format(folder))
+    if remove.failed:
+        status = False
+
+    remove = run('rm -rf /data/web_static/current')
+    if remove.failed:
+        status = False
+
+    link = run('ln -s {} /data/web_static/current'.format(folder))
+    if link.failed:
+        status = False
+
+    return status
