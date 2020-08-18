2e4b7ab9841809b9242a2f208ed8090c46e56bff
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-18 00:17:11 +0000

    create file
    fabric script
    
    script that generate a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a Fabric script """
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ script that generates a .tgz archive """
+    date_now = datetime.utcnow()
+    year = date_now.year
+    month = date_now.month
+    day = date_now.day,
+    hour = date_now.hour,
+    minute = date_now.minute,
+    second = date_now.second
+
+    file_date = "version/web_static_{}{}{}{}{}{}.tgz".format(year,
+                                                             month,
+                                                             day,
+                                                             hour,
+                                                             minute,
+                                                             second)
+
+    local("mkdir -p version")
+    local("tar -cvzf {} web_static".format(file_date))
+
+    return (file_date)
