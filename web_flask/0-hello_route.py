954ee4315bdcec9712244a233d3f4f65772310c0
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-31 11:31:07 -0500

    create files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask
+""" script that starts a Flask web application """
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display “Hello HBNB!” """
+    return ("Hello HBNB!")
+
+if __name__ == '__main__':
+    """ runserver port 5000 """
+    app.run(debug=True, port=5000, host='0.0.0.0')
