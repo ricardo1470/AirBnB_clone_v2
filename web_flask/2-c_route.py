d098d24c728d3f5635ead6adbe1a8c0758fc02c6
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-31 11:53:19 -0500

    script that starts a Flask web application
    display “C ” followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask
+""" script that starts a Flask web application
+    Your web application must be
+    listening on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB” """
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display “Hello HBNB!” """
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display “HBNB!” """
+    return ("HBNB")
+
+@app.route('/c/<text>/', strict_slashes=False)
+def c(text = "value", str = "nothing"):
+    return ('C {}'.format(text.replace("_", "")))
+
+
+if __name__ == '__main__':
+    """ run server port 5000 """
+    app.run(debug=True, port=5000, host='0.0.0.0')
