448890df8fc28dfefd73e4718c5861aff3ba1678
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-09-01 16:57:45 -0500

    update file

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python3
-from flask import Flask
 """ script that starts a Flask web application
     Your web application must be
     listening on 0.0.0.0, port 5000
     Routes:
     /: display “Hello HBNB!”
-    /hbnb: display “HBNB” 
+    /hbnb: display “HBNB”
     /c/<text>: display “C ” followed by the value
     of the text variable
     /python/(<text>): display “Python ”,
     followed by the value of the text variable.
     /number/<n>: display “n is a number” only if n is an integer """
+from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     """ display “Hello HBNB!” """
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display “HBNB!” """
     return ("HBNB")
 
+
 @app.route('/c/<text>/', strict_slashes=False)
-def c(text = "value"):
+def c(text="value"):
     """ display “C ” followed by the value of the text variable """
     return ('C {}'.format(text.replace("_", " ")))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>/', strict_slashes=False)
-def python(text = "is cool"):
+def python(text="is cool"):
     """ display “Python ”, followed by the value of the text variable """
     return ('Python {}'.format(text.replace("_", " ")))
 
@@ -42,6 +46,7 @@ def python(text = "is cool"):
 def number(n):
     """ display “n is a number” only if n is an integer """
     return ('{} is a number'.format(n))
+
 
 if __name__ == '__main__':
     """ run server port 5000 """
