c9a49c4c60257e8e506bf2469fcb68803c7b7031
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-09-01 16:53:43 -0500

    update file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from flask import Flask
 """ script that starts a Flask web application
     Your web application must be
     listening on 0.0.0.0, port 5000
@@ -10,24 +9,29 @@ from flask import Flask
      of the text variabl
     /python/(<text>): display “Python ”,
     followed by the value of the text variable """
+from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     """ display “Hello HBNB!” """
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display “HBNB!” """
     return ("HBNB")
 
+
 @app.route('/c/<text>/', strict_slashes=False)
 def c(text = "value"):
     """ display “C ” followed by the value of the text variable """
     return ('C {}'.format(text.replace("_", " ")))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>/', strict_slashes=False)
