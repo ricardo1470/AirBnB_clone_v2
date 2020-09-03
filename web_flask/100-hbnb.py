0cbf0868a91ba1bf4bf747c3bb2915083d5533a4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-09-03 00:01:29 -0500

    update file

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def states_list(id='0'):
+    """ Display HTML page with list of states """
+    return render_template('100-hbnb.html',
+                           states=storage.all(State).values(),
+                           amenities=storage.all(Amenity).values(),
+                           places=storage.all(Place).values(), id=id)
+
+
+@app.teardown_appcontext
+def remove_session(response_or_exc):
+    """ Remove the current SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """ run server port 5000 """
+    app.run(debug=True, port=5000, host='0.0.0.0')
